7c20d6f8e2fe22c50947d3f9f235adf3c581a0ea
bubblefish-dev <daforsastudia@gmail.com>
2022-04-09 11:56:53 +0300

    2

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
+const body = document.getElementsByTagName('body')[0];
 const toggles = [...document.getElementsByClassName('button-toggle')];
 toggles.forEach(toggle => {
-    
+    toggle.addEventListener('click', _ => {
+        if (!body.classList.contains('toggled')) body.classList.add('toggled');
+        else body.classList.remove('toggled');
+    });
 });
