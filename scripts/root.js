52c4b27656d79b1f1742f1ee43f3a282fb1b0f94
bubblefish-dev <daforsastudia@gmail.com>
2022-04-15 18:55:43 +0300

    Update

--- scripts/root.js
+++
@@ -1,9 +0,0 @@
-[...document.getElementsByClassName('category')].forEach((element, _) => {
-    element.addEventListener('click', () => {
-        if (element.classList.contains('revealed')) {
-            element.classList.remove('revealed');
-        } else {
-            element.classList.add('revealed');
-        }
-    });
-});
--- test/test.js
+++
@@ -1,8 +0,0 @@
-const body = document.getElementsByTagName('body')[0];
-const toggles = [...document.getElementsByClassName('button-toggle')];
-toggles.forEach(toggle => {
-    toggle.addEventListener('click', _ => {
-        if (!body.classList.contains('toggled')) body.classList.add('toggled');
-        else body.classList.remove('toggled');
-    });
-});
